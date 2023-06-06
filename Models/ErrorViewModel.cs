5c6f3273a44add13f9cc9e50a2557a5f5a5b7cf9
Abdul rasheed Bello <akindats1@gmail.com>
2023-06-06 03:59:20 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComplaintRequestSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
