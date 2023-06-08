10ff262883c01c3ec8163c7089402d70720a971d
Abdul rasheed Bello <akindats1@gmail.com>
2023-06-08 01:38:10 +0100

    Model updated

--- Models/Department/CreateDepartmentViewModel.cs
+++ Models/Department/CreateDepartmentViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace ComplaintRequestSystem.Models.Department
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComplaintRequestSystem.Models.Department
 {
     public class CreateDepartmentViewModel
     {
+
+        [Required(ErrorMessage = "Department name is required")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
     }
 }
