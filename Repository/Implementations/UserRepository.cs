bd6da62104e8509df4021c2071a8c689b0679160
Abdul rasheed Bello <akindats1@gmail.com>
2023-06-08 01:53:28 +0100

    Repository updated

---
+++ Repository/Implementations/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using ComplaintRequestSystem.Context;
+using ComplaintRequestSystem.Entities;
+using ComplaintRequestSystem.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ComplaintRequestSystem.Repository.Implementations
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(ComplaintRequestSystemContext context) : base(context)
+        {
+        }
+
+        public User GetUser(Expression<Func<User, bool>> expression)
+        {
+            return _context.Users
+                .Include(r => r.Role)
+                .SingleOrDefault(expression);
+        }
+    }
+}
