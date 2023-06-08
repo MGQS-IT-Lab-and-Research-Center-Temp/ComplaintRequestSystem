9f2cb357d58b2d0038f30a0d8d7fcb1554c99787
Abdul rasheed Bello <akindats1@gmail.com>
2023-06-08 01:54:27 +0100

    Services added to the container

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
 using ComplaintRequestSystem.Context;
+using ComplaintRequestSystem.Repository.Implementations;
+using ComplaintRequestSystem.Repository.Interfaces;
+using ComplaintRequestSystem.Service.Implementations;
+using ComplaintRequestSystem.Service.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IComplaintRepository, ComplaintRepository>();
+builder.Services.AddScoped<IComplaintService, ComplaintService>();
+builder.Services.AddScoped<IRequestRepository, RequestRepository>();
+builder.Services.AddScoped<IRequestService, RequestService>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ComplaintRequestSystemContext>(option =>
     option.UseMySQL(builder.Configuration.GetConnectionString("ComplaintRequestSystemContext")));
