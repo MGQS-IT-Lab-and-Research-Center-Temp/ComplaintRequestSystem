cd87d16e765bde1e1cf9b9cc9c84e726d5ebb3be
Abdul rasheed Bello <akindats1@gmail.com>
2023-06-19 20:57:36 +0100

    Removed Email implementation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -139,43 +139,7 @@ namespace ComplaintRequestSystem.Controllers
             return View();
         }
 
-        [HttpPost]
-        public async Task<ActionResult> Contact(EmailFormModel model)
-        {
-            if (ModelState.IsValid && User.IsInRole("admin"))
-            {
-                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
-                var message = new MailMessage();
-                message.To.Add(new MailAddress("akin@gmail.com"));  // replace with valid value 
-                message.From = new MailAddress("admin@gmail.com");  // replace with valid value
-                message.Subject = "Your email subject";
-                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
-                message.IsBodyHtml = true;
 
-                using (var smtp = new SmtpClient())
-                {
-                    var credential = new NetworkCredential
-                    {
-                        UserName = "akin@gmail.com",  
-                        Password = "akin"  
-                    };
-                    smtp.Credentials = credential;
-                    smtp.Host = "smtp-mail.gmail.com";
-                    smtp.Port = 587;
-                    smtp.EnableSsl = true;
-                    await smtp.SendMailAsync(message);
-                    return RedirectToAction("Sent");
-                }
-            }
-            return View(model);
-
-            
-        }
-
-        public IActionResult Sent()
-        {
-            return View();
-        }
     }
 
    
